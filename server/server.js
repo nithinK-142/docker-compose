6aedcd126e1b68ffe972df54f9e21c195476b8d0
nithinK-142 <nithin.sagar.359@gmail.com>
2025-11-09 13:19:02 +0530

    update: dockerize server

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(cors({ origin: "*" }))
 
 app.get("/", (req, res) => res.send("Server is up & running!!"))
 
-app.get("/hello", (req, res) => res.json({ message: "Hello from Server" }))
+app.get("/hello", (req, res) => res.json({ message: "👋Hello from Server" }))
 
 const PORT = process.env.PORT || "3300"
 app.listen(PORT, () => console.log(`Server is up & running on http://localhost:${PORT}`))
